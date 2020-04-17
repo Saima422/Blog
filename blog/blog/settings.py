5c8291a62434fdb6a1d946fdb2f2d1d451ee4eec
Saima422 <sayedssaima04@gmail.com>
2020-04-17 17:34:35 +0530

    commit-blog-done

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -24,9 +24,9 @@ LOGOUT_REDIRECT_URL = '/'
 SECRET_KEY = '!v&!tfbtrcb+2^tzj#j+34dnnw81k^*w1q-72llsilo#h09ejs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
